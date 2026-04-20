f8b789187bd56bb86e0031f6b0f6b7798d6d2327
null <null>
2026-04-20 12:00:50 +0200

    v.01.01.002

--- src/main.js
+++ src/main.js
@@ -2,6 +2,31 @@ import './style.css';
 import { supabase } from './supabase.js'; // Додаємо фігурні дужки
 import './nav.js';
 
-const money = document.getElementById('money')
+
 const familyMoney = document.getElementById('familyMoney')
 
+async function displayMoney() {
+
+    const money = document.getElementById('content_money');
+
+    const { data, error } = await supabase
+        .from('money')
+        .select('*');
+
+        if (error) {
+            money.innerText = "Помилка: " + error.message;
+            return;
+        }
+        money.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+}
+displayMoney();
+
+supabase
+  .channel('any-name-here') 
+  .on('postgres_changes', 
+      { event: '*', schema: 'public', table: 'money' }, 
+      (payload) => {
+          displayMoney(); // Перемальовуємо дані
+      }
+  )
+  .subscribe();
